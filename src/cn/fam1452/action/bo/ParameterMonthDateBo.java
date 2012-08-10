315901b627c0d9f8cf9e969730fd582d20ac515b
gls_ghy <gelishun2005@163.com>
2012-08-10 10:15:18 +0800

    hh

---
+++ src/cn/fam1452/action/bo/ParameterMonthDateBo.java
@@ -0,0 +1,179 @@
+package cn.fam1452.action.bo;
+
+public class ParameterMonthDateBo {
+	private String days;
+	private String h00 ;
+	private String h01 ;
+	private String h02 ;
+	private String h03 ;
+	private String h04 ;
+	private String h05 ;
+	private String h06 ;
+	private String h07 ;
+	private String h08 ;
+	private String h09 ;
+	private String h10 ;
+	private String h11 ;
+	private String h12 ;
+	private String h13 ;
+	private String h14 ;
+	private String h15 ;
+	private String h16 ;
+	private String h17 ;
+	private String h18 ;
+	private String h19 ;
+	private String h20 ;
+	private String h21 ;
+	private String h22 ;
+	private String h23 ;
+	public String getDays() {
+		return days;
+	}
+	public void setDays(String days) {
+		this.days = days;
+	}
+	public String getH00() {
+		return h00;
+	}
+	public void setH00(String h00) {
+		this.h00 = h00;
+	}
+	public String getH01() {
+		return h01;
+	}
+	public void setH01(String h01) {
+		this.h01 = h01;
+	}
+	public String getH02() {
+		return h02;
+	}
+	public void setH02(String h02) {
+		this.h02 = h02;
+	}
+	public String getH03() {
+		return h03;
+	}
+	public void setH03(String h03) {
+		this.h03 = h03;
+	}
+	public String getH04() {
+		return h04;
+	}
+	public void setH04(String h04) {
+		this.h04 = h04;
+	}
+	public String getH05() {
+		return h05;
+	}
+	public void setH05(String h05) {
+		this.h05 = h05;
+	}
+	public String getH06() {
+		return h06;
+	}
+	public void setH06(String h06) {
+		this.h06 = h06;
+	}
+	public String getH07() {
+		return h07;
+	}
+	public void setH07(String h07) {
+		this.h07 = h07;
+	}
+	public String getH08() {
+		return h08;
+	}
+	public void setH08(String h08) {
+		this.h08 = h08;
+	}
+	public String getH09() {
+		return h09;
+	}
+	public void setH09(String h09) {
+		this.h09 = h09;
+	}
+	public String getH10() {
+		return h10;
+	}
+	public void setH10(String h10) {
+		this.h10 = h10;
+	}
+	public String getH11() {
+		return h11;
+	}
+	public void setH11(String h11) {
+		this.h11 = h11;
+	}
+	public String getH12() {
+		return h12;
+	}
+	public void setH12(String h12) {
+		this.h12 = h12;
+	}
+	public String getH13() {
+		return h13;
+	}
+	public void setH13(String h13) {
+		this.h13 = h13;
+	}
+	public String getH14() {
+		return h14;
+	}
+	public void setH14(String h14) {
+		this.h14 = h14;
+	}
+	public String getH15() {
+		return h15;
+	}
+	public void setH15(String h15) {
+		this.h15 = h15;
+	}
+	public String getH16() {
+		return h16;
+	}
+	public void setH16(String h16) {
+		this.h16 = h16;
+	}
+	public String getH17() {
+		return h17;
+	}
+	public void setH17(String h17) {
+		this.h17 = h17;
+	}
+	public String getH18() {
+		return h18;
+	}
+	public void setH18(String h18) {
+		this.h18 = h18;
+	}
+	public String getH19() {
+		return h19;
+	}
+	public void setH19(String h19) {
+		this.h19 = h19;
+	}
+	public String getH20() {
+		return h20;
+	}
+	public void setH20(String h20) {
+		this.h20 = h20;
+	}
+	public String getH21() {
+		return h21;
+	}
+	public void setH21(String h21) {
+		this.h21 = h21;
+	}
+	public String getH22() {
+		return h22;
+	}
+	public void setH22(String h22) {
+		this.h22 = h22;
+	}
+	public String getH23() {
+		return h23;
+	}
+	public void setH23(String h23) {
+		this.h23 = h23;
+	}
+}
